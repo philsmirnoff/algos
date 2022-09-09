3c3c13b44e9f3999321b22f4bbd3e3aeef181dbd
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-09-09 02:06:25 -0400

    minSubArrLen

--- arrays/threeSum.js
+++ arrays/threeSum.js
@@ -93,3 +93,49 @@ arrayTwoSum([11,-1], -10 + 0)`
   }
   return solution
 }`
+
+
+var threeSum = function(nums) {
+  nums.sort((a, b) => a-b)
+  
+  const result = []
+
+  
+  for(let i = 0; i < nums.length; i++){
+  //only run on the first instance of a number, skip to remaining to avoid duplicates
+      if(i > 0 && nums[i] === nums[i-1]){
+          continue
+      }
+     
+
+      
+      //run 2sum on remainder of the sorted array 
+      let l = i + 1
+      let r = nums.length - 1
+      
+      while(l < r){
+          if(nums[l] + nums[r] + nums[i] === 0){
+              result.push([nums[i], nums[l], nums[r]])
+          }else if(nums[l] + nums[r] + nums[i] > 0){
+              r--
+              continue
+          }else if(nums[l] + nums[r] + nums[i] < 0){
+              l++
+              continue
+          }
+          
+          l++
+
+          while(l < nums.length && nums[l] === nums[l-1]){
+              l++
+          }
+          
+      }
+  }
+  
+  
+  return result
+};
+/*
+
+[-2, 0, 1, 1, 2]ß
