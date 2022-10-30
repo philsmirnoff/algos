3b7d580e20a4aaaf3bb66a086ebadd2fbd3a7669
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-10-30 03:13:18 -0400

    fruits into the busket

--- 1. grokking_the-coding_interview/sliding_window/5.fuits_into_busket.js
+++ 1. grokking_the-coding_interview/sliding_window/5.fuits_into_busket.js
@@ -58,3 +58,42 @@ console.log(`Maximum number of fruits: `
 
 // Space Complexity
 // The algorithm runs in constant space O(1) as there can be a maximum of three types of fruits stored in the frequency map.
+
+
+function non_repeat_substring(str) {
+  let windowStart = 0,
+      maxLength = 0,
+      charIndexMap = {};
+
+  // try to extend the range [windowStart, windowEnd]
+  for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+      const rightChar = str[windowEnd];
+      // if the map already contains the 'rightChar', shrink the window from the beginning
+      // so that we have only one occurrence of 'rightChar'
+      if (rightChar in charIndexMap) {
+          // this is tricky; in the current window, we will not have any 'rightChar' after
+          // its previous index and if 'windowStart' is already ahead of the last index of
+          // 'rightChar', we'll keep 'windowStart'
+          windowStart = Math.max(windowStart, charIndexMap[rightChar] + 1);
+      }
+      // insert the 'rightChar' into the map
+      charIndexMap[rightChar] = windowEnd;
+      // remember the maximum length so far
+      maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+  }
+  return maxLength;
+}
+
+
+console.log(`Length of the longest substring: ${non_repeat_substring('aabccbb')}`);
+console.log(`Length of the longest substring: ${non_repeat_substring('abbbb')}`);
+console.log(`Length of the longest substring: ${non_repeat_substring('abccde')}`);
+
+
+// Time Complexity
+
+// The above algorithm’s time complexity will be O(N)O(N), where ‘N’ is the number of characters in the input string.
+
+// Space Complexity
+
+// The algorithm’s space complexity will be O(K)O(K), where KK is the number of distinct characters in the input string. This also means K<=NK<=N, because in the worst case, the whole string might not have any duplicate character, so the entire string will be added to the HashMap. Having said that, since we can expect a fixed set of characters in the input string (e.g., 26 for English letters), we can say that the algorithm runs in fixed space O(1)O(1); in this case, we can use a fixed-size array instead of the HashMap.
