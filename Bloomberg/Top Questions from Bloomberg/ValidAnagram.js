0efc15636225e5777d7c73824cd953bd0e36f72c
Ilfir Yakupov <philsmirnoff@gmail.com>
2023-02-18 00:54:10 -0500

    valid anagram

---
+++ Bloomberg/Top Questions from Bloomberg/ValidAnagram.js
@@ -0,0 +1,45 @@
+// Valid Anagram
+
+// Solution
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+
+// Input: s = "rat", t = "car"
+// Output: false
+
+
+const isAnagram = (s, t) => {
+  if (s.length !== t.length) return false;
+  let count = {};
+
+  for (let char of s) {
+    if (!(count[char])) {
+      count[char] = 0;
+    }
+    count[char]++;
+  }
+
+  for (let char of t) {
+    if (!(count[char])) {
+      return false;
+    }
+    count[char]--;
+  }
+
+  for (let char in count) {
+    if (count[char] !== 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
