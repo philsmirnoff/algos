f6f64f3266755a89fb801db1f42bdeef8775aa5d
Ilfir Yakupov <philsmirnoff@gmail.com>
2023-04-13 23:58:36 -0400

    updates

--- Bloomberg/Top Questions from Bloomberg/DynamicProgramming/LongestPolindromicString.js
+++ Bloomberg/Top Questions from Bloomberg/DynamicProgramming/LongestPolindromicString.js
@@ -33,12 +33,12 @@ function longestPalindrome(s) {
   }
   return s.slice(start, start + maxLength); // return the longest palindromic substring
 }
-We define a function called longestPalindromicSubstring that takes a string s as input.
-We initialize two variables start and maxLength to keep track of the starting index and length of the longest palindromic substring found so far.
-We define a nested function expandAroundCenter that takes two indices left and right as input. This function will be used to expand around a center character and check for palindromes.
-Within the expandAroundCenter function, we use a while loop to keep expanding outwards from the center character as long as the characters on the left and right are equal and within the bounds of the string.
-At each step of the while loop, we check if the current palindrome length is greater than the current maxLength. If so, we update start and maxLength.
-We then decrement left and increment right to continue expanding outwards until the while loop condition is no longer satisfied.
-We then use a for loop to iterate through each character in the string and call expandAroundCenter twice for each character. The first call handles odd length palindromes by treating the character as the center. The second call handles even length palindromes by treating the character and the next character as the center.
-Once the for loop has completed, we return the longest palindromic substring by slicing the string from start to start + maxLength.
-Overall, this solution has a time complexity of O(n^2) because we iterate over each character in the string and call expandAroundCenter twice for each character. However, because we are only expanding around a center character and not checking every possible substring, the actual runtime is much faster in practice.
+// We define a function called longestPalindromicSubstring that takes a string s as input.
+// We initialize two variables start and maxLength to keep track of the starting index and length of the longest palindromic substring found so far.
+// We define a nested function expandAroundCenter that takes two indices left and right as input. This function will be used to expand around a center character and check for palindromes.
+// Within the expandAroundCenter function, we use a while loop to keep expanding outwards from the center character as long as the characters on the left and right are equal and within the bounds of the string.
+// At each step of the while loop, we check if the current palindrome length is greater than the current maxLength. If so, we update start and maxLength.
+// We then decrement left and increment right to continue expanding outwards until the while loop condition is no longer satisfied.
+// We then use a for loop to iterate through each character in the string and call expandAroundCenter twice for each character. The first call handles odd length palindromes by treating the character as the center. The second call handles even length palindromes by treating the character and the next character as the center.
+// Once the for loop has completed, we return the longest palindromic substring by slicing the string from start to start + maxLength.
+// Overall, this solution has a time complexity of O(n^2) because we iterate over each character in the string and call expandAroundCenter twice for each character. However, because we are only expanding around a center character and not checking every possible substring, the actual runtime is much faster in practice.
