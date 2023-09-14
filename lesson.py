c3e7d4634c49960d717b731ff7b315d0ebed006c
Ilfir Yakupov <philsmirnoff@gmail.com>
2023-09-14 00:22:01 -0400

    1029.TwoCityScheduling

---
+++ lesson.py
@@ -0,0 +1,16 @@
+# numberOfLoops = 0
+# numberOfApples = 2
+# while numberOfLoops < 10:
+#     numberOfApples *= 2
+#     numberOfApples += numberOfLoops
+#     numberOfLoops -= 1
+# print("Number of apples: " + str(numberOfApples))
+
+num = 10
+while True:
+    if num < 7:
+        print('Breaking out of loop')
+        break
+    print(num)
+    num -= 1
+print('Outside of loop')
