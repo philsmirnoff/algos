8d71ff53e2c57a44dd90e8473def676fe4092bff
Ilfir Yakupov <philsmirnoff@gmail.com>
2025-04-16 00:22:39 -0400

    MaximumSubbaray.js

---
+++ BLIND75/Greedy/MaxSubbarra.js
@@ -0,0 +1,14 @@
+const maxSubArray = (nums) => {
+  let max = nums[0]
+  let sum = nums[0]
+
+  for (let i = 1; i < nums.length; i++) {
+      if (sum + nums[i] > nums[i]) {
+          sum+=nums[i]
+      } else {
+          sum = nums[i]
+      }
+      max = Math.max(max, sum)
+  }
+  return max
+}
