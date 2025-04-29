b13dc8b518d4c5f8fbdd3dd9cca11d57c2e12d73
Ilfir Yakupov <philsmirnoff@gmail.com>
2025-04-29 18:25:52 -0400

    updated solution for 208. Implement tree

--- BLIND75/Tries/208. ImplementTrie.js
+++ BLIND75/Tries/208. ImplementTrie.js
@@ -109,3 +109,54 @@ Trie.prototype.startsWith = function(prefix) {
  * var param_2 = obj.search(word)
  * var param_3 = obj.startsWith(prefix)
  */
+
+
+
+var Trie = function() {
+    this.root = {}; // Initialize the root as an empty object
+};
+
+/**
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function(word) {
+    let node = this.root;
+    for (let char of word) {
+        if (!node[char]) {
+            node[char] = {}; // Create a new node if it doesn't exist
+        }
+        node = node[char]; // Move to the next node
+    }
+    node.isEnd = true; // Mark the end of a word
+};
+
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function(word) {
+    let node = this.root;
+    for (let char of word) {
+        if (!node[char]) {
+            return false; // If at any point, character not found
+        }
+        node = node[char];
+    }
+    return node.isEnd === true; // Only return true if it's an end of a word
+};
+
+/**
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function(prefix) {
+    let node = this.root;
+    for (let char of prefix) {
+        if (!node[char]) {
+            return false; // If prefix path breaks
+        }
+        node = node[char];
+    }
+    return true; // Prefix fully matched
+};
